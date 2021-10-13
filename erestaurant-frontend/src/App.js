b4408a00c87ae0cd59faeb0c23058896912ee1d0
Cass Francisco <cassandra.francisco@student.uts.edu.au>
2021-10-13 15:23:47 +1100

    deleting node modules and updating app.js

--- erestaurant-frontend/src/App.js
+++ erestaurant-frontend/src/App.js
@@ -1,24 +1,24 @@
 import "./App.css";
 import { HomePage } from "./containers/HomePage";
 import { buildPath, HomePagePath } from "./Paths";
-import { LoginPage } from "./containers/CustomerAccessPage/customerLoginPage";
-import { LoggedInPage } from "./containers/CustomerAccessPage/loggedInPage";
-import { StaffLoginPage } from "./containers/CustomerAccessPage/staffLoginPage";
-import { RegisterPage } from "./containers/CustomerAccessPage/registerPage";
-import { RegisteredPage } from "./containers/CustomerAccessPage/registeredPage";
-import { OrderPage } from "./containers/CustomerAccessPage/orderPage";
+import { LoginPage } from "./containers/customerAccessPage/customerLoginPage";
+import { LoggedInPage } from "./containers/customerAccessPage/loggedInPage";
+import { StaffLoginPage } from "./containers/customerAccessPage/staffLoginPage";
+import { RegisterPage } from "./containers/customerAccessPage/registerPage";
+import { RegisteredPage } from "./containers/customerAccessPage/registeredPage";
+import { OrderPage } from "./containers/customerAccessPage/orderPage";
 import { ListStaffPage } from "./containers/StaffAccessPage/listStaffPage";
 import { EditStaff } from "./containers/StaffAccessPage/editStaff";
 import { AddStaff } from "./containers/StaffAccessPage/addStaff";
-import { BookingPage } from "./containers/CustomerAccessPage/bookingPage";
-import { BookedPage } from "./containers/CustomerAccessPage/bookedPage";
-import { CustomerAccountDetailsPage } from "./containers/CustomerAccessPage/customerAccountDetailsPage"
-import { CustomerAccountPage } from "./containers/CustomerAccessPage/customerAccountPage"
+import { BookingPage } from "./containers/customerAccessPage/bookingPage";
+import { BookedPage } from "./containers/customerAccessPage/bookedPage";
+import { CustomerAccountDetailsPage } from "./containers/customerAccessPage/customerAccountDetailsPage"
+import { CustomerAccountPage } from "./containers/customerAccessPage/customerAccountPage"
 import { MenuViewPage } from "./containers/MenuPage/MenuViewPage";
 import { AboutPage } from "./containers/HomePage/aboutPage";
-import { Invoice } from "./containers/CustomerAccessPage/invoice";
+import { Invoice } from "./containers/customerAccessPage/invoice";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { OrderPaymentPage } from "./containers/CustomerAccessPage/OrderPaymentPage";
+import { OrderPaymentPage } from "./containers/customerAccessPage/OrderPaymentPage";
 
 function App() {
   return (
